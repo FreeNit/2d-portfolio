a9171f4bdba3e745de99308468e065e4104664cd
Dimon <bolilui.dmytro@gmail.com>
2025-02-09 16:38:42 +0200

    added dialog text

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,21 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript | TypeScript | React.js.
+    I've made a couple of games in that language. I am learning Web3, AI, Cloud and game dev with JS 🧐. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/FreeNit" target="_blank">Github</a>!`,
+  "cs-degree": `This is my AWS cloud practitioner certificate. I hung it on the wall because I'm proud of it.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@WebDevSimplified" target="_blank">WebDevSimplified</a>, <a href="https://www.youtube.com/@cosdensolutions" target="_blank">Cosden Solutions</a>! and sometimes <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org(🔥)</a>`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it document with link to my LinkedIn profile. <a href="https://www.linkedin.com/in/dmytro-kovach/" target="_blank">My LinkedIn profile</a>
+  Contact me at LinkedIn if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching JSLegendDev tutorials <a href="https://www.youtube.com/@jslegenddev" target="_blank">here</a> and reading Koboom.JS documentation <a href="https://kaboomjs.com/doc/setup" target="_blank">here</a>`,
+  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { k } from "./kaboomCtx";
-import { scaleFactor } from "./constants";
+import { dialogueData, scaleFactor } from "./constants";
 import { displayDialogue, setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
@@ -9,7 +9,7 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     "idle-down": 936,
     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
     "idle-side": 975,
-    "walk-down": { from: 975, to: 978, loop: true, speed: 8 },
+    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
     "idle-up": 1014,
     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
   },
@@ -55,7 +55,10 @@ k.scene("main", async () => {
         if (boundary.name) {
           player.onCollide(boundary.name, () => {
             player.isInDialogue = true;
-            displayDialogue("TODO", () => (player.isInDialogue = false));
+            displayDialogue(
+              dialogueData[boundary.name],
+              () => (player.isInDialogue = false)
+            );
           });
         }
       }
@@ -92,6 +95,57 @@ k.scene("main", async () => {
 
     const worldMousePos = k.toWorld(k.mousePos());
     player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim() !== "walk-up"
+    ) {
+      player.play("walk-up");
+      player.direction = "up";
+      return;
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== "walk-down"
+    ) {
+      player.play("walk-down");
+      player.direction = "down";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+      return;
+    }
+
+    if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+      return;
+    }
+  });
+
+  k.onMouseRelease(() => {
+    if (player.direction === "down") {
+      player.play("idle-down");
+      return;
+    }
+    if (player.direction === "up") {
+      player.play("idle-up");
+      return;
+    }
+    player.play("idle-side");
   });
 });
 
